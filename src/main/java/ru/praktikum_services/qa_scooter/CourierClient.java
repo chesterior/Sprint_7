bbf571f1643b8abee0f825dca39f7410ed916c4e
asafronov <asafronov@sberhealth.ru>
2023-04-30 17:44:54 +0300

    данные с вебинара

--- src/main/java/ru/praktikum_services/qa_scooter/CourierClient.java
+++ src/main/java/ru/praktikum_services/qa_scooter/CourierClient.java
@@ -1,17 +1,28 @@
 package ru.praktikum_services.qa_scooter;
 
 import io.restassured.response.ValidatableResponse;
-import static io.restassured.RestAssured.given;
-public class CourierClient extends RestClient{
-    private static final String COURIER_PATH = "/api/v1/courier";
 
-    public ValidatableResponse create(Courier courier){
-        return (ValidatableResponse) given()
+import static io.restassured.RestAssured.given;
+
+public class CourierClient extends RestClient {
+    private static final String COURIER_PATH = "/api/v1/courier";
+    private static final String COURIER_LOGIN_PATH = "/api/v1/courier/login";
+
+    public ValidatableResponse create(Courier courier) {
+        return given()
                 .spec(getBaseSpec())
                 .body(courier)
                 .when()
                 .post(COURIER_PATH)
-                .then()
-                .extract();
+                .then();
+    }
+
+    public ValidatableResponse login(CourierCredentials credentials) {
+        return given()
+                .spec(getBaseSpec())
+                .when()
+                .body(credentials)
+                .post(COURIER_LOGIN_PATH)
+                .then();
     }
 }
--- src/test/java/ru/praktikum_services/qa_scooter/CourierCreateTest.java
+++ src/test/java/ru/praktikum_services/qa_scooter/CourierCreateTest.java
@@ -1,4 +1,20 @@
 package ru.praktikum_services.qa_scooter;
 
+import io.restassured.response.ValidatableResponse;
+import org.junit.Test;
+
 public class CourierCreateTest {
+    @Test
+    public void courierCanBeCreated() {
+        Courier courier = new Courier();
+        CourierClient courierClient = new CourierClient();
+
+        ValidatableResponse createResponse = courierClient.create(courier);
+
+        int statusCode = createResponse.extract().statusCode();
+        boolean isCouriersCreated = createResponse.extract().path("ok");
+
+        ValidatableResponse loginResponse = courierClient.login(CourierCredentials.from(courier));
+        int courierId = loginResponse.extract().path("id");
+    }
 }
